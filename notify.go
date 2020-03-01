aecc8736e92cadf51bd3ac966327f3f7f91a8ed1
Igor Komlew <igor.komlew@gmail.com>
2020-03-01 20:53:29 +0100

    move interface to core package

--- notify.go
+++ notify.go
@@ -3,21 +3,13 @@ package notify
 import (
 	"context"
 
+	"github.com/sevigo/notify/core"
 	"github.com/sevigo/notify/event"
 	"github.com/sevigo/notify/watcher"
 )
 
-// DirectoryWatcher interface
-type DirectoryWatcher interface {
-	Event() chan event.Event
-	Error() chan event.Error
-	Scan(path string) error
-	StartWatching(path string)
-	StopWatching(path string)
-}
-
 // Setup returns a channel for file change notifications and errors
-func Setup(ctx context.Context, options *watcher.Options) DirectoryWatcher {
+func Setup(ctx context.Context, options *watcher.Options) core.DirectoryWatcher {
 	eventCh := make(chan event.Event)
 	errorCh := make(chan event.Error)
 	if options == nil {
